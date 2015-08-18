c1735237a9c8a1919de016d4a1d91e7f797e4056
Bartosz Bubak <bartosz.bubak@gmail.com>
2015-08-18 02:02:21 +0200

    Nice looking (and working) GUI !!!

--- src/main/java/org/ibu/rpgforge/plotgui/HelloController.java
+++ src/main/java/org/ibu/rpgforge/plotgui/HelloController.java
@@ -2,20 +2,68 @@ package org.ibu.rpgforge.plotgui;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import org.ibu.rpgforge.jplotgenerator.PlotParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javafx.event.ActionEvent;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
 
 public class HelloController implements Initializable
 {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
     @FXML private WebView mainWebView;
     @FXML private ListView mainNPCList;
+    @FXML private Button mainRefreshButton;
+    @FXML private TextArea mainSourceTextArea;
+
+    public class TextFileReader {
+
+        @SuppressWarnings("NestedAssignment")
+        public String read(File file) {
+            List<String> lines = new ArrayList<String>();
+            String result = "";
+            String line;
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+
+                while ((line = br.readLine()) != null) {
+                    //lines.add(line);
+                    result += line + "\n";
+                }
+                br.close();
+            } catch (IOException ex) {
+                //Logger.getLogger(TextFileReader.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            return result;
+        }
+    }
+
+    @FXML
+    public void mainFrameRefresh(ActionEvent event) {
+        PlotParser parser = new PlotParser("CDD4");
+        this.webEngine.loadContent(parser.parse(this.mainSourceTextArea.getText()));
+    }
+
+    @FXML
+    public void sourceTextAreaLoad(ActionEvent event) {
+        this.mainSourceTextArea.setText(new TextFileReader().read(new File("CDD4/bb_random.npc")));
+    }
 
     private WebEngine webEngine;
 
